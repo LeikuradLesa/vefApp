721a4b9b4acbf1ea6cf14ebdd69c0610a8b8816e
Andres Haukur <haukurandres@gmail.com>
2023-11-09 09:11:03 +0000

    DB setup finished inline to DBSchema.pdf

--- dbSetup.sql
+++ dbSetup.sql
@@ -2,8 +2,6 @@
 
 
 -- create Tables in the database leikuradlesa
-
-delimiter $$
 -- create table notandi that stores all user info
 create table notandi(
     notendanafn varchar(50) not null,
@@ -13,18 +11,62 @@ create table notandi(
     simanumer varchar(50) not null,
     dob DATE not null,
     tegundnotanda int not null,
-    primary key(notendanafn)
+    primary key(notendanafn),
     FOREIGN KEY (tegundnotanda) REFERENCES TegundNotanda(tegundnotanda)
-)$$
+)
 
 -- create table TegundNotanda that stores all user types
 create table TegundNotanda(
     tegundnotanda int not null,
     tegund varchar(50) not null,
     primary key(tegundnotanda)
-)$$
+)
 
--- create table hopur that has all groups of studnets and their teachers
+insert into TegundNotanda values(0, "Kennari");
+insert into TegundNotanda values(1, "Nemandi");
+
+-- create table hopur that has all groups of students and their teachers
+
+create table Hopur {
+    ID int not null,
+    nafnhops varchar(255),
+    notendaID varchar(50) not null,
+    primary key(ID),
+    FOREIGN KEY (notendaID) REFERENCES notandi(notendanafn)
+}
+
+-- create table SpurningaTegundir that stores the different types of questions
+create table SpurningaTegundir(
+    tegund int not null,
+    tegundspurningar varchar(50) not null,
+    primary key(tegund)
+)
+
+insert into SpurningaTegundir values(0, "Fjölvalsspurning");
+insert into SpurningaTegundir values(1, "Myndaspurning");
+insert into SpurningaTegundir values(2, "Textaspurning");
+
+-- create table Bok that stores the different books and info on that book
+create table Bok(
+    ID int not null,
+    nafnbokar varchar(255) not null,
+    hofundur varchar(255) not null,
+    utgafuar varchar(4) not null,
+    primary key (ID)
+)
+
+-- create table spurningar that stores the different questions
+create table spurningar (
+    ID int not null,
+    spurning varchar(255) not null,
+    rettSvar varchar(255) not null,
+    spurningategund int not null,
+    bokID int not null,
+    primary key(ID),
+    FOREIGN KEY (spurningategund) REFERENCES SpurningaTegundir(tegund),
+    FOREIGN KEY (bokID) REFERENCES Bok(ID)
+)
+
 
 
 
