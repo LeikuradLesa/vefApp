0052a9eb5bc205a980b269e285f6077159e8d960
Heimir1231 <heimirgud20@tskoli.is>
2023-11-20 11:31:53 +0000

    gera css fyrir login síðuna

--- vefsida/src/components/Login.tsx
+++ vefsida/src/components/Login.tsx
@@ -1,5 +1,5 @@
-
 import React, { useState } from 'react';
+import './Login.css'; // Import the CSS file
 
 const LoginPage: React.FC = () => {
   const [username, setUsername] = useState('');
@@ -21,23 +21,24 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="login-container"> {/* Add a class name for styling */}
+      <h1>Innskráning nemanda</h1>
       <form onSubmit={handleLogin}>
         <label>
-          Username:
+          Notendanafn:  <br />
           <input type="text" value={username} onChange={handleUsernameChange} />
         </label>
         <br />
         <label>
-          Password:
+          Lykilorð: <br />
           <input type="password" value={password} onChange={handlePasswordChange} />
         </label>
         <br />
-        <button type="submit">Login</button>
+        <button type="submit">Innskrá</button>
       </form>
     </div>
   );
 };
 
 export default LoginPage;
+
