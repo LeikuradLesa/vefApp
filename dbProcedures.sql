73f7cc17cc7278a1041f6c90a9f3049fcebb2f48
Andres Haukur <haukurandres@gmail.com>
2023-11-21 14:17:41 +0000

    lagað login procedure

--- dbProcedures.sql
+++ dbProcedures.sql
@@ -1,35 +1,17 @@
--- Active: 1699365169542@@leikuradlesa.cxafacplwecg.eu-north-1.rds.amazonaws.com@3306@LeikuradLesa
 
--- Create a procedure to check if the notendanafn and lykilord is correct and has a tegundnotanda = 0
--- if the password and notendanafn is correct return 1 and if not return -1
-DELIMITER //
-CREATE PROCEDURE CheckKennari(IN p_password VARCHAR(50), IN p_username VARCHAR(50))
+-- Create a stored procedure that takes in a users notendanafna and lykilord and it checks the tegundnotanda to see if a user is a kennari or a nemandi
+-- if the user is a kennari it returns 1 if a nemandi it returns 0 and if anything elsa it returns -1
+
+DELIMITER $$
+CREATE PROCEDURE Login(IN p_username VARCHAR(50), IN p_password VARCHAR(50))
 BEGIN
-    DECLARE v_tegundnotanda INT;
+    DECLARE v_tegundnotanda INT DEFAULT -1;
+    
     SELECT tegundnotanda INTO v_tegundnotanda FROM notandi WHERE notendanafn = p_username AND lykilord = p_password;
-    IF v_tegundnotanda = 0 THEN
-        SELECT 1 AS result;
-    ELSE
-        SELECT -1 AS result;
-    END IF;
-END //
-DELIMITER ;
 
--- Create a procedure to check if the notendanafn and lykilord is correct and has a tegundnotanda = 1
--- if the password and notendanafn is correct return 1 and if not return -1
-DELIMITER //
-CREATE PROCEDURE CheckNemandi(IN p_password VARCHAR(50), IN p_username VARCHAR(50))
-BEGIN
-    DECLARE v_tegundnotanda INT;
-    SELECT tegundnotanda INTO v_tegundnotanda FROM notandi WHERE notendanafn = p_username AND lykilord = p_password;
-    IF v_tegundnotanda = 1 THEN
-        SELECT 1 AS result;
-    ELSE
-        SELECT -1 AS result;
-    END IF;
-END //
+    SELECT v_tegundnotanda AS result;
+END $$
 DELIMITER ;
-
 
 
 
