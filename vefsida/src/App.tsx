1f2de97fcba81958f64a295ff2006c36eac4e662
Hrannar <hrannarh269@gmail.com>
2023-11-27 10:40:27 +0000

    klara burgermenu

--- vefsida/src/App.tsx
+++ vefsida/src/App.tsx
@@ -1,31 +1,19 @@
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Nemandi from "./components/VefsidaNemanda";
-import Navbar from "./components/navbar";
 
 function App() {
-
   return (
     <div className="App">
-      <Navbar
-        items={items}
-        heading="Leikur að lesa"
-        onSelectItem={handleSelectectItem}
-      />
       <Router>
-      <div>
-        
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/Nemandi" element={<Nemandi />} />
-
-        </Routes>
-        
-      </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/Nemandi" element={<Nemandi />} />
+          </Routes>
+        </div>
       </Router>
     </div>
-    
   );
 }
 
