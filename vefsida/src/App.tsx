65965749f95fb79fe27ca41d74a0c6fb862b1157
Heimir1231 <heimirgud20@tskoli.is>
2023-11-27 09:54:29 +0000

    laga burger

--- vefsida/src/App.tsx
+++ vefsida/src/App.tsx
@@ -1,25 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ListGroup from "./components/ListGroup";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import AboutUs from "./components/about-us";
 import Homepage from "./components/Homepage";
 
 function App() {
-  let items = ["Um okkur", "Hafðu samband", "Leiðbeningar", "Inskráning"];
-
-  const handleSelectectItem = (item: string) => {
-    console.log(item);
-  };
 
   return (
     <Router>
       <div>
-        <ListGroup
-          items={items}
-          heading="Leikur að lesa"
-          onSelectItem={handleSelectectItem}
-        />
+        
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Login />} />
