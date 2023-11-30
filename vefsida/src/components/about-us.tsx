cb7c14f21dbde409fa2fd407f1d7be54d77e9a01
Heimir1231 <heimirgud20@tskoli.is>
2023-11-30 08:50:04 +0000

    smá lagfæring

--- vefsida/src/components/about-us.tsx
+++ vefsida/src/components/about-us.tsx
@@ -6,7 +6,7 @@ const AboutUs: React.FC = () => {
     <div className="about-us">
       <h2>Um okkur</h2>
       <p>Velkominn á síðuna okkar Leiku að lesa.</p>
-      <p>Við erum nemendur á Tölvubraut í tækniskólanum og við fengum það verkefni að hanna vefsíðu.</p>
+      <p>Við erum nemendur á Tölvubraut í Tækniskólanum og við fengum það verkefni að hanna vefsíðu.</p>
       <p>Þannig að við vildum færa ykkur þessa vefsíðu sem að gerir það að leiki að lesa</p>
     </div>
   );
