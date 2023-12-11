7b09a0a62e719e810f5417e5c965000cbf88bcaf
arnar1234 <arnark060606@gmail.com>
2023-12-11 10:46:43 +0000

    Bruh

--- vefsida/src/components/Velgert.tsx
+++ vefsida/src/components/Velgert.tsx
@@ -2,14 +2,8 @@ import React from "react";
 import Footer from "./footer";
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import Navbar from "./navbar";
 
 const Velgert: React.FC = () => {
-    let items = ["Um okkur", "Hafðu samband", "Leiðbeningar", "Inskráning", "útskráning"];
-    const handleSelectectItem = (item: any) => {
-        console.log(item);
-    };
-
     const location = useLocation();
     const user = location.state as { username: string } | undefined;
     const username = user?.username
